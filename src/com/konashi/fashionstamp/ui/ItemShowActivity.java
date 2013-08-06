7325fdee66885706780959a007e17037aefe797c
Yudai <y.shinnoki@gmail.com>
2013-08-06 16:25:40 +0900

    Android Manufest

---
+++ src/com/konashi/fashionstamp/ui/ItemShowActivity.java
@@ -0,0 +1,11 @@
+package com.konashi.fashionstamp.ui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class ItemShowActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
