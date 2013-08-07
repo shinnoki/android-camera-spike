3c0cfdce3109c09d20750a73120f97b1cd1e7d74
hi-sa <hironicus1230@gmail.com>
2013-08-07 23:03:01 +0900

    fix

--- src/com/konashi/fashionstamp/ui/MainActivity.java
+++ src/com/konashi/fashionstamp/ui/MainActivity.java
@@ -2,10 +2,8 @@ package com.konashi.fashionstamp.ui;
 
 import android.annotation.SuppressLint;
 import android.app.Activity;
-import android.app.AlertDialog;
 import android.content.ContentResolver;
 import android.content.ContentValues;
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.net.Uri;
@@ -13,7 +11,6 @@ import android.os.Bundle;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -28,6 +25,7 @@ public class MainActivity extends Activity implements OnClickListener {
     
     private Uri mSaveUri;
 
+	@SuppressLint("NewApi")
 	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
